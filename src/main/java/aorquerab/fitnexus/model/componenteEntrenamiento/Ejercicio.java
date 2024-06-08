3b611275e787d0c47f976f3276c5373e3eeccc4b
a.orquerab <a.orquerab.sde@gmail.com>
2024-06-08 14:06:12 +0200

    Refactor and added JsonIgnore @ to avoid infinite serialization when creating new exercise

--- src/main/java/aorquerab/fitnexus/model/componenteEntrenamiento/Ejercicio.java
+++ src/main/java/aorquerab/fitnexus/model/componenteEntrenamiento/Ejercicio.java
@@ -23,27 +23,22 @@ public class Ejercicio {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    //@NotEmpty(message = "Nombre no puede ser vacío y el tamaño debe ser mayor que 0!")
     private String nombreEjercicio;
 
-    //@Positive(message = "Número de repeticiones no debe ser negativo!")
     private Integer repeticion;
 
-    //@Positive (message = "Número de series no debe ser negativo!")
     private Integer serie;
 
-    //@Positive (message = "Peso no debe ser negativo!")
     private Integer peso;
 
-    //cardioRealizado allow us to use Ejercicio for cardio exercises
     private boolean cardioRealizado;
 
-    //If using ExerciseRepository extends ListCrudRepository (if using JDBC)
     @Version
     private Integer version;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "entrenador_id", nullable = false)
+    @JsonIgnore //To avoid infinite serialization
     private Entrenador entrenador;
 
     @ManyToMany(mappedBy = "ejercicios", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
