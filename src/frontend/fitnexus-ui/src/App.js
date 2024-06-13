35acbc862ccccaa822218e20a16b2d94b308a923
a.orquerab <a.orquerab.sde@gmail.com>
2024-06-13 19:05:55 +0200

    Added first 2 components, button from cli and button2 manually

--- src/frontend/fitnexus-ui/src/App.js
+++ src/frontend/fitnexus-ui/src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import React, { Component } from "react";
 import './App.css';
+import { Button } from "./components_ui/ui/button"
+import { Loader2 } from "lucide-react"
 
 class App extends Component {
     state = {
@@ -19,6 +21,11 @@ class App extends Component {
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
+            <Button variant="outline">Button</Button>
+    <Button disabled>
+      <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+      Please wait
+    </Button>
             <div className="App-intro">
                 <h2>Ejercicios</h2>
                 {ejercicios.map(ejercicio =>
