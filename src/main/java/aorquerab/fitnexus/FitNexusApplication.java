659b79cc14c480702a0341229627907a8c5409d8
a.orquerab <a.orquerab.sde@gmail.com>
2024-07-19 12:21:16 +0200

    Fixed issue loading contextLoad after adding DB config in application.yml

--- src/main/java/aorquerab/fitnexus/FitNexusApplication.java
+++ src/main/java/aorquerab/fitnexus/FitNexusApplication.java
@@ -2,13 +2,9 @@ package aorquerab.fitnexus;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
-@EnableAutoConfiguration (exclude = { DataSourceAutoConfiguration.class , JpaRepositoriesAutoConfiguration.class})
 @Slf4j
 public class FitNexusApplication {
 
--- src/test/java/aorquerab/fitnexus/FitNexusApplicationTests.java
+++ src/test/java/aorquerab/fitnexus/FitNexusApplicationTests.java
@@ -6,7 +6,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class FitNexusApplicationTests {
 
-	//TODO: Fix context loading due to datasource configuration?
 	@Test
 	void contextLoads() {
 	}
