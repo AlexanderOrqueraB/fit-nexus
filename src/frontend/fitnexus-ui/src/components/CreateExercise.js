0910bae26bfb907bf2bee2cbbb11e1253a6273b6
a.orquerab <a.orquerab.sde@gmail.com>
2024-09-03 19:21:06 +0200

    WIP Post Ejercicio Axios React

--- src/frontend/fitnexus-ui/src/components/CreateExercise.js
+++ src/frontend/fitnexus-ui/src/components/CreateExercise.js
@@ -1,4 +1,8 @@
+import { waitFor } from "@testing-library/react";
+import axios from "axios";
 import { Button } from "../components_ui/ui/button"
+import React, { useRef, useState, useEffect } from "react"; //(2)
+
 
 import {
   Dialog,
@@ -12,15 +16,11 @@ import {
 import { Input } from "../components_ui/ui/input"
 import { Label } from "../components_ui/ui/label"
 
+const wait = () => new Promise((resolve) => setTimeout(resolve, 1000));
+
 export function CreateExercise() {
 
-    const [data, setData] = useState({ //useState to store data from server
-        nombreEjercicio: "",
-        repeticion: "",
-        serie:"",
-        peso: "",
-        cardioRealizado: ""
-    });
+/*     
 
     const handleChange = (e) => {
         const value = e.target.value;
@@ -30,19 +30,7 @@ export function CreateExercise() {
         });
     }
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        const userData = {
-            nombreEjercicio: data.nombreEjercicio,
-            repeticion: data.repeticion,
-            serie: data.serie,
-            peso: data.peso ,
-            cardioRealizado: data.cardioRealizado
-        };
-        axios.post("http://localhost:8080/api/ejercicios", userData).then((response) => {
-          console.log(response.status, response.data.token);
-        });
-      };
+     */
 
 
 //    const api = axios.create({
@@ -66,6 +54,35 @@ export function CreateExercise() {
 //        fetchData();
 //    }, []) //empty array ensures that the effect only runs once
 
+      const printEFE = (e) => {
+        console.log('hola aAsfsdf');
+        
+      }
+
+      useState ({});
+      const [data, setData] = useState({ //useState to store data from server
+        nombreEjercicio: "",
+        repeticion: "",
+        serie:"",
+        peso: "",
+        cardioRealizado: ""
+    });
+
+      const onSubmit = (e) => {
+        e.preventDefault();
+        const userData = {
+            nombreEjercicio: data.nombreEjercicio,
+            repeticion: data.repeticion,
+            serie: data.serie,
+            peso: data.peso ,
+            cardioRealizado: data.cardioRealizado
+        };
+        axios.post("http://localhost:8080/api/ejercicios", userData)
+        .then((response) => {
+          console.log(response.status, response.data.token);
+        }).catch(console.error("Axios post error"));
+      };
+
   return (
 
   //    <ul>
@@ -86,71 +103,71 @@ export function CreateExercise() {
       <DialogTrigger asChild>
         <Button variant="outline">Crear ejercicio</Button>
       </DialogTrigger>
-      <DialogContent className="sm:max-w-[425px]">
-        <DialogHeader>
-          <DialogTitle>Crear ejercicio </DialogTitle>
-          <DialogDescription>
-            Crea un ejercicio aquí.
-          </DialogDescription>
-          <DialogDescription>
-            Haz click en Guardar cuando hayas terminado.
-          </DialogDescription>
-        </DialogHeader>
-        <div className="grid gap-4 py-4">
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="name" className="text-right">
-              Nombre del ejercicio
-            </Label>
-            <Input
-              id="name"
-              defaultValue="Press banca"
-              className="col-span-3"
-            />
-          </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="repeticion" className="text-right">
-              Repeticiones
-            </Label>
-            <Input
-              id="repeticion"
-              defaultValue="5"
-              className="col-span-3"
-            />
-          </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-              <Label htmlFor="serie" className="text-right">
-                Series
+      <DialogContent className="sm:max-w-[425px]"> 
+          <DialogHeader>
+            <DialogTitle>Crear ejercicio </DialogTitle>
+            <DialogDescription>
+              Crea un ejercicio aquí.
+            </DialogDescription>
+            <DialogDescription>
+              Haz click en Guardar cuando hayas terminado.
+            </DialogDescription>
+          </DialogHeader>
+          <div className="grid gap-4 py-4">
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="name" className="text-right">
+                Nombre del ejercicio
               </Label>
               <Input
-                id="serie"
+                id="name"
+                defaultValue="Press banca"
+                className="col-span-3"
+              />
+            </div>
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="repeticion" className="text-right">
+                Repeticiones
+              </Label>
+              <Input
+                id="repeticion"
                 defaultValue="5"
                 className="col-span-3"
               />
+            </div>
+            <div className="grid grid-cols-4 items-center gap-4">
+                <Label htmlFor="serie" className="text-right">
+                  Series
+                </Label>
+                <Input
+                  id="serie"
+                  defaultValue="5"
+                  className="col-span-3"
+                />
+            </div>
+            <div className="grid grid-cols-4 items-center gap-4">
+                <Label htmlFor="peso" className="text-right">
+                  Peso (kg)
+                </Label>
+                <Input
+                  id="peso"
+                  defaultValue="30"
+                  className="col-span-3"
+                />
+            </div>
+            <div className="grid grid-cols-4 items-center gap-4">
+                <Label htmlFor="cardioRealizado" className="text-right">
+                  Cardio (Si, no)
+                </Label>
+                <Input
+                  id="cardioRealizado"
+                  defaultValue="No"
+                  className="col-span-3"
+                />
+            </div>
           </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-              <Label htmlFor="peso" className="text-right">
-                Peso (kg)
-              </Label>
-              <Input
-                id="peso"
-                defaultValue="30"
-                className="col-span-3"
-              />
-          </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-              <Label htmlFor="cardioRealizado" className="text-right">
-                Cardio (Si, no)
-              </Label>
-              <Input
-                id="cardioRealizado"
-                defaultValue="No"
-                className="col-span-3"
-              />
-          </div>
-        </div>
-        <DialogFooter>
-          <Button type="submit">Guardar cambios</Button>
-        </DialogFooter>
+          <DialogFooter>
+            <Button onClick={onSubmit} type="submit">Guardar cambios</Button>
+          </DialogFooter>
       </DialogContent>
     </Dialog>
   )
