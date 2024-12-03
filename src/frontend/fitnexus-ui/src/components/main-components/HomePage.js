3cd0e74be2bd0f46ec036d9baf4df326931a4546
a.orquerab <a.orquerab.sde@gmail.com>
2024-12-04 00:46:01 +0100

    WIP: Create components for Plan de entrenamiento and Rutina + added buttons to home (testing purpose)

--- src/frontend/fitnexus-ui/src/components/main-components/HomePage.js
+++ src/frontend/fitnexus-ui/src/components/main-components/HomePage.js
@@ -6,6 +6,8 @@ import ChangePassword from './ChangePassword';
 import { Toaster, toast } from 'sonner'
 import { Button } from "../../components_ui/ui/button"
 import ExercisePost from './ExercisePost';
+import WIPRoutinePost from './WIPRoutinePost';
+import WIPPlanPost from './WIPPlanPost';
 
 export function HomePage() {
 
@@ -26,17 +28,20 @@ export function HomePage() {
 			</div>
 			<h3>No tienes aun añadidos tus datos extra para poder calcular tu dieta! Añadelos pulsando el botón</h3>
 			<div>
-				<EditProfile />
-				<EditProfileExtra />
-				<ChangePassword />
+				<div className="flex flex-row space-x-4"> 
+					<EditProfile />
+					<EditProfileExtra />
+					<ChangePassword />
+					<Toaster expand={false} position="top-right" richColors closeButton  />
+					<Button onClick={confirmationToast} type="submit">Confirmation</Button>
+					<Button onClick={errorToast} type="submit">Error</Button>
+				</div>
 
-				<Toaster expand={false} position="top-right" richColors closeButton  />
-				<Button onClick={confirmationToast} type="submit">Confirmation</Button>
-      			<Button onClick={errorToast} type="submit">Error</Button>
-
-
-				<ExercisePost/>
-				
+				<div className="flex flex-row space-x-4"> 
+					<ExercisePost/>
+					<WIPPlanPost/>
+					<WIPRoutinePost/>
+				</div>
 			</div>
 		</div>
 	);
